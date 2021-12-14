871a48de0ea8dd217a3f5682ee4b951472f09631
maikerucode <michaeljoshuasarmiento@gmail.com>
2021-12-14 13:49:56 +0800

    get and setEmployeeId() not used
    get and setEmployeeID() in:
    /Entity

--- src/main/java/com/csa/samplefullstack/Entity/Employee.java
+++ src/main/java/com/csa/samplefullstack/Entity/Employee.java
@@ -5,7 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-//import lombok.Data;
+import lombok.Data;
+
 
 /**
  *
